dd1251f64841305c75179fdaef4d7df0ec10e431
Noah Zemlin <noah.zemlin@ou.edu>
2017-10-07 20:52:32 -0500

    Updated GuardAI Added Guard and Bullet prefabs

--- Assets/GuardScript.cs
+++ Assets/GuardScript.cs
@@ -7,6 +7,11 @@ public class GuardScript : MonoBehaviour {
     public GameObject playerView;
     public LayerMask playerMask;
 
+    public float visionRange = 10f;
+    public float fov = 130f;
+
+    public GameObject bulletPrefab;
+
 	// Use this for initialization
 	void Start () {
         /*
@@ -16,20 +21,44 @@ public class GuardScript : MonoBehaviour {
         test.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
         */
 	}
+
+    void Shoot(Vector3 dir)
+    {
+        GameObject bullet = Instantiate(bulletPrefab);
+        bullet.transform.position = this.transform.position;
+        bullet.GetComponent<Rigidbody>().velocity = dir.normalized * 5f;
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        //The vector going from the guard to the player
         Vector3 dispVector = playerView.transform.position - this.transform.position;
-        if (dispVector.magnitude <= 10)
+        
+        //if vector is outside range, ignore.
+        if (dispVector.magnitude <= visionRange)
         {
+            //do raycast from guard to person
             RaycastHit[] hits = Physics.RaycastAll(this.transform.position, dispVector, dispVector.magnitude);
-            if (hits.Length == 1 && Vector3.Angle(this.transform.forward, dispVector) < 45)
+            int count = 0;
+            foreach( RaycastHit hit in hits)
             {
+                if (!hit.collider.CompareTag("Transparenct"))
+                {
+                    count++;
+                }
+            }
+
+            //if hits.length == 1, then only the player was on the vector, aka the guard can see the player
+            if (hits.Length == 1 && Vector3.Angle(this.transform.forward, dispVector) < fov/2)
+            {
+                //set red if detected
                 this.GetComponent<Renderer>().material.color = new Color(0.8f, 0.1f, 0.0f);
+                Shoot(dispVector);
                 return;
             }
         }
 
+        //set to green by default
         this.GetComponent<Renderer>().material.color = new Color(0.1f, 0.8f, 0.0f);
     }
 }
