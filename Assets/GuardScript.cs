ff9f90bdfb128144d28c303bc9a53ed40e43a10d
Noah Zemlin <Noah Zemlin>
2017-10-08 18:58:28 -0500

    Added Map, Guard, and Player Controls -Uses VRTK and SteamVR for view and controls -Museum map added with few textures -One painting that can be moved around

--- Assets/GuardScript.cs
+++ Assets/GuardScript.cs
@@ -7,10 +7,16 @@ public class GuardScript : MonoBehaviour {
     public GameObject playerView;
     public LayerMask playerMask;
 
+    public GameObject coloryThingy;
+
     public float visionRange = 10f;
     public float fov = 130f;
 
     public GameObject bulletPrefab;
+
+    private float lastShot;
+
+    private bool isOn = true;
 
 	// Use this for initialization
 	void Start () {
@@ -20,19 +26,29 @@ public class GuardScript : MonoBehaviour {
         test.transform.position = this.transform.position + this.transform.forward / 10f;
         test.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
         */
+        lastShot = Time.time;
 	}
 
     void Shoot(Vector3 dir)
     {
-        GameObject bullet = Instantiate(bulletPrefab);
-        bullet.transform.position = this.transform.position;
-        bullet.GetComponent<Rigidbody>().velocity = dir.normalized * 5f;
+        if (Time.time - lastShot > 0.8f)
+        {
+            GameObject bullet = Instantiate(bulletPrefab);
+            bullet.transform.position = this.transform.position + this.transform.forward * this.transform.lossyScale.x / 1.4f;
+            bullet.GetComponent<Rigidbody>().velocity = dir.normalized * 20f;
+            lastShot = Time.time;
+        }
     }
 	
 	// Update is called once per frame
 	void Update () {
+        if (!isOn)
+        {
+            return;
+        }
         //The vector going from the guard to the player
         Vector3 dispVector = playerView.transform.position - this.transform.position;
+        Debug.DrawRay(this.transform.position, this.transform.forward);
         
         //if vector is outside range, ignore.
         if (dispVector.magnitude <= visionRange)
@@ -42,23 +58,30 @@ public class GuardScript : MonoBehaviour {
             int count = 0;
             foreach( RaycastHit hit in hits)
             {
-                if (!hit.collider.CompareTag("Transparenct"))
+                if (!hit.collider.CompareTag("Transparent"))
                 {
                     count++;
                 }
             }
 
             //if hits.length == 1, then only the player was on the vector, aka the guard can see the player
-            if (hits.Length == 1 && Vector3.Angle(this.transform.forward, dispVector) < fov/2)
+            if (count == 0 && Vector3.Angle(this.transform.forward, dispVector) < fov/2)
             {
                 //set red if detected
-                this.GetComponent<Renderer>().material.color = new Color(0.8f, 0.1f, 0.0f);
+                coloryThingy.GetComponent<Renderer>().material.color = new Color(0.8f, 0.1f, 0.0f);
                 Shoot(dispVector);
                 return;
             }
         }
 
         //set to green by default
-        this.GetComponent<Renderer>().material.color = new Color(0.1f, 0.8f, 0.0f);
+        coloryThingy.GetComponent<Renderer>().material.color = new Color(0.1f, 0.8f, 0.0f);
+
+    }
+
+    public void SetOnOff(float x)
+    {
+        Debug.Log(x);
+        isOn = x < 50;
     }
 }
---
+++ Assets/LeverScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+using VRTK.UnityEventHelper;
+
+public class LeverScript : MonoBehaviour {
+
+    public GuardScript script;
+    private VRTK_Control_UnityEvents controlEvents;
+
+    private void Start()
+    {
+        controlEvents = GetComponent<VRTK_Control_UnityEvents>();
+        if (controlEvents == null)
+        {
+            controlEvents = gameObject.AddComponent<VRTK_Control_UnityEvents>();
+        }
+
+        controlEvents.OnValueChanged.AddListener(HandleChange);
+    }
+
+    private void HandleChange(object sender, Control3DEventArgs e)
+    {
+        script.SetOnOff(e.normalizedValue);
+    }
+}
